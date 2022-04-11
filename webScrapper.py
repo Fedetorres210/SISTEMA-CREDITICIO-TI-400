38102cab73c78b4a554c6666263d36c7eec941d3
Federico Torres <federicotorres2109@gmail.com>
2022-04-10 21:09:55 -0600

    Feat:Api_Example

--- webScrapper.py
+++ webScrapper.py
@@ -1,7 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-TBIURL = "https://gee.bccr.fi.cr/indicadoreseconomicos/cuadros/frmvercatcuadro.aspx?idioma=1&codcuadro=%2017"
+def webScrapper(url,selector):
+    res = requests.get(url)
+    html = BeautifulSoup(res.text)
+    table =  html.select(selector)
+    return table
 
-res = requests.get(TBIURL)
-print(res.text)
+def separadorColumnasYDatos(table):
+    columns = []
+    data = []
+    for elem in table:
+        try:
+            if int(elem.get_text()) > 2000:
+                columns.append(elem.get_text())
+        except:
+            data.append(elem.get_text().replace(",","."))
+    return columns, data
+
+def separadorData(datos):
+    data = []
+    index = []
+    for elem in datos:
+        try:
+            if float(elem) > 0:
+                data.append(elem)
+        except:
+            if elem != '':
+                index.append(elem)
+    return index, data
+
+
+
+def creacionMatriz(data):
+    matrix =  []
+    index = 11
+    while index < len(data):
+        if len(matrix) >= len(data[4018:])+3:
+            break
+        matrix.append(data[index])
+        index += 12
+    return matrix
+
+
+
